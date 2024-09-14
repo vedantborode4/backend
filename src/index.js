9777736bf52c9de5ccd7c099623d66ea8b802931
Vedant <borodevedant4@gmail.com>
2024-09-14 23:59:10 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@
 
 import dotenv from "dotenv"
 import connetDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
 connetDB()
+.then(() => {
+    // app.on("error",(error)=>{
+    //     console.log("Error", error);
+    //     throw error  
+    // },
+    app.listen(process.env.PORT || 8000, (error) => {
+        try {
+            console.log(`Server is running at port: ${process.env.PORT}`)
+        } catch (error) {
+            console.log('ERROR IN RUNNING SERVER ON PORT', process.env.PORT);
+        }
+    })
+})
+.catch((error) => {
+    console.log("MONGO DB connection failed !!!", error);
+})
+
 
 
 /*
