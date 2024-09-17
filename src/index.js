f946f8e8c94a4fe41e536bec40a397cefdeb89c2
Vedant <borodevedant4@gmail.com>
2024-09-17 18:05:53 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -14,12 +14,14 @@ connetDB()
     //     console.log("Error", error);
     //     throw error  
     // },
-    app.listen(process.env.PORT || 8000, (error) => {
-        try {
-            console.log(`Server is running at port: ${process.env.PORT}`)
-        } catch (error) {
-            console.log('ERROR IN RUNNING SERVER ON PORT', process.env.PORT);
-        }
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at PORT : ${process.env.PORT}`);
+        
+        // try {
+        //     console.log(`Server is running at port: ${process.env.PORT}`)
+        // } catch (error) {
+        //     console.log('ERROR IN RUNNING SERVER ON PORT', process.env.PORT);
+        // }
     })
 })
 .catch((error) => {
